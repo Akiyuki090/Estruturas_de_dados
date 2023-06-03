d91248627e5b8806f1d8df3ef106f68af50403a4
Akiyuki <akiyuki090@gmail.com>
2023-06-03 12:57:41 -0300

    segundo commit

---
+++ Pilhas/main.py
@@ -0,0 +1,31 @@
+'''
+Criação de uma pilha com todos os métodos
+'''
+class Pilha:
+    def __init__(self):
+        self.items = []
+
+    def is_vazia(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_vazia():
+            return self.items.pop()
+        else:
+            raise IndexError("A pilha está vazia")
+
+    def peek(self):
+        if not self.is_vazia():
+            return self.items[-1]
+        else:
+            raise IndexError("A pilha está vazia")
+
+    def tamanho(self):
+        return len(self.items)
+
+if "_name"=="main_":
+    minhaPilha = Pilha()
+    
--- Vetor_Ordenado/main.py
+++ Vetor_Ordenado/main.py
@@ -1,3 +1,37 @@
 '''
 Criação de um vetor ordenado com todos os métodos
 '''
+
+class vetor_ordenado:
+    def _init_(self):
+        self.vetor = []
+    
+    def adicionarElemento(self, elemento):
+        posicao = 0
+        while posicao < len(self.vetor) and self.vetor[posicao] < elemento:
+            posicao += 1
+        self.vetor.append(None)
+        for i in range(len(self.vetor) - 1, posicao, -1):
+            self.vetor[i] = self.vetor[i - 1]
+        self.vetor[posicao] = elemento
+    
+    def pesquisaElemento(self, elemento):
+        for i in range(len(self.vetor)):
+            if self.vetor[i] == elemento:
+                print(f'Valor encontrado na posição: {i}')
+            elif self.vetor[i] > elemento:
+                print(f'Valor: {elemento} não encontrado no vetor ordenado')
+    
+    def excluiElemento(self, elemento):
+        if elemento in self.vetor:
+            self.vetor.remove(elemento)
+            print(f'O valor {elemento} foi removido com sucesso !!!')
+        else:
+            print(f'O valor {elemento} não foi encontrado na lista !!!')
+
+    def verVetor(self):
+        print(self.vetor)
+
+if "_name"=="main_":
+    meuVetor = vetor_ordenado()
+    
